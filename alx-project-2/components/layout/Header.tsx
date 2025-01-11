004e33260cc4b748a5ce6cc1179b6d65bcc09fa7
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2025-01-11 21:41:19 +0100

    Add page routing

--- alx-project-2/components/layout/Header.tsx
+++ alx-project-2/components/layout/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-blue-600 p-4">
+    <header className="bg-blue-600 p-4 flex justify-between items-center">
       <h1 className="text-white text-3xl font-bold">Welcome to alx-project-2</h1>
+      <nav>
+        <Link href="/home" className="text-white mr-4">Home</Link>
+        <Link href="/about" className="text-white">About</Link>
+      </nav>
     </header>
   );
 };
---
+++ alx-project-2/pages/home.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <h1 className="text-4xl">Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
