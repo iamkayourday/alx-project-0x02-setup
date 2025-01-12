c76ce0e1ba3ea1222e556e122ee7f51c1daae46b
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2025-01-12 10:55:24 +0100

    Reusable Card Component

--- alx-project-2/pages/home.tsx
+++ alx-project-2/pages/home.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Card from '../components/common/Card';
+
 
 const Home = () => {
   return (
@@ -7,6 +9,10 @@ const Home = () => {
         <title>Home</title>
       </Head>
       <h1 className="text-4xl">Welcome to the Home Page</h1>
+      <Card title="Card 1" content="This is the content of Card 1" />
+      <Card title="Card 2" content="This is the content of Card 2" />
+      <Card title="Card 3" content="This is the content of Card 3" />
+
     </div>
   );
 };
