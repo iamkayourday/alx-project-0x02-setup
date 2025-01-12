7240bc605d4671f52e8bf883bac6dcc75d02a2a3
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2025-01-12 11:15:11 +0100

    Modal Component

--- alx-project-2/pages/home.tsx
+++ alx-project-2/pages/home.tsx
@@ -1,18 +1,33 @@
 import Head from 'next/head';
+import React, { useState } from 'react';
+
 import Card from '../components/common/Card';
+import PostModal from '../components/common/postModal';
 
+const Home: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [posts, setPosts] = useState<{ title: string; content: string }[]>([]);
 
-const Home = () => {
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSavePost = (title: string, content: string) => {
+    setPosts([...posts, { title, content }]);
+  };
+
   return (
     <div>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <h1 className="text-4xl">Welcome to the Home Page</h1>
-      <Card title="Card 1" content="This is the content of Card 1" />
-      <Card title="Card 2" content="This is the content of Card 2" />
-      <Card title="Card 3" content="This is the content of Card 3" />
-
+      <h1>Home Page</h1>
+      <button onClick={handleOpenModal}>Create Post</button>
+      <PostModal isOpen={isModalOpen} onClose={handleCloseModal} onSave={handleSavePost} />
+      {posts.map((post, index) => (
+        <Card key={index} title={post.title} content={post.content} />
+      ))}
     </div>
   );
 };
